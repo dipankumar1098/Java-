1e8c0331fd0dd8ca1db2791774b50cb756c45d75
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-25 21:44:34 +0530

    add

---
+++ B.java
@@ -0,0 +1,14 @@
+class B
+{
+int a=10;
+static double b=10.5;
+public static void main(String args[])
+{
+boolean c=true;
+B s=new B();
+System.out.println(s.a);
+System.out.println(b);
+System.out.println(c);
+}
+}
+
